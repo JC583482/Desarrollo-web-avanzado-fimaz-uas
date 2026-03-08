5afea83d43189e8c47b7a63474f48cd33f66b125
JC583482 <jesusyibranc@gmail.com>
2026-03-08 02:41:10 -0700

    correccion de sintaxis:  por Usuario

--- parcial-1-poo/practica-1/index.php
+++ parcial-1-poo/practica-1/index.php
@@ -1,7 +1,7 @@
 <?php
 Require 'Usuario.php';
 
-$iUsuario = new $usuario ("Yibran", "abcd@gmail.com");
+$iUsuario = new Usuario ("Yibran", "abcd@gmail.com");
 
 echo "Nombre: ". $user->getNombre() . "<br>";
 echo "Correo: ". $user->getCorreo();
